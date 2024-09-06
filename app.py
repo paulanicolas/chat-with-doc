44cb62719ef213a9693360b2114af8d75b089d9d
Paula <paula.nicolas@speeki.com>
2024-09-06 14:40:52 +0800

    Added cost and number of tokens

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from langchain_community.embeddings import BedrockEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from openai import OpenAI
+from anthropic_bedrock import AnthropicBedrock
 
 # Initialize Bedrock client
 bedrock_us_east_1 = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
@@ -26,6 +27,43 @@ bedrock_us_west_2 = boto3.client(service_name="bedrock-runtime", region_name="us
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
+
+# Token cost per model (cost per 1000 tokens)
+token_costs = {
+    "anthropic.claude-3-5-sonnet-20240620-v1:0": {"input": 0.003 / 1000, "output": 0.015 / 1000},
+    "anthropic.claude-3-haiku-20240307-v1:0": {"input": 0.00025 / 1000, "output": 0.00125 / 1000}
+}
+
+def count_tokens_claude(prompt):
+    """Count the number of tokens in a Claude prompt (using a simple word count as a proxy)."""
+    #return len(prompt.split())
+    return len(prompt) // 4
+
+def calculate_cost(model_id, input_tokens, output_tokens):
+    """Calculate total cost based on token usage for Claude models."""
+    input_cost = token_costs[model_id]["input"] * input_tokens
+    output_cost = token_costs[model_id]["output"] * output_tokens
+    total_cost = input_cost + output_cost
+    return total_cost
+
+def calculate_cost_claude(model_id, input_prompt, output):
+    input_tokens = count_tokens_claude(input_prompt)
+    output_tokens = count_tokens_claude(output)
+
+    # Calculate cost based on token usage
+    total_cost = calculate_cost(model_id, input_tokens, output_tokens)
+
+    return total_cost, input_tokens, output_tokens
+
+def calculate_cost_claude_new(model_id, input_prompt, output):
+    client = AnthropicBedrock()
+    input_tokens = client.count_tokens(input_prompt)
+    output_tokens = client.count_tokens(output)
+    
+    # Calculate cost based on token usage
+    total_cost = calculate_cost(model_id, input_tokens, output_tokens)
+
+    return total_cost, round(input_tokens), round(output_tokens)
 
 def data_ingestion(file_path):
     """Load and split PDF documents into chunks."""
@@ -81,9 +119,9 @@ def get_content_of_vectore_store(query):
     return doc.page_content
 
 
-def get_claude_llm():
+def get_claude_llm(model_id):
     """Initialize Claude LLM."""
-    llm = BedrockChat(model_id="anthropic.claude-3-5-sonnet-20240620-v1:0", client=bedrock_us_east_1)
+    llm = BedrockChat(model_id=model_id, client=bedrock_us_east_1)
     return llm
 
 def get_ai21_llm():
@@ -141,6 +179,7 @@ prompt_template_llama = """
 The following is a conversation between a human and an AI assistant.
 The assistant is designed to provide concise, accurate, and precise answers related to Environmental, Social, and Governance (ESG) topics. The assistant will not include any additional commentary, chit-chat, or unnecessary information. It will answer the questions directly and clearly. Below are examples of the type of answers expected:
 
+<example>
 Example Questions and Answers:
 
 Q: What is the reporting period in which the climate-related scenario analysis was carried out?  
@@ -154,10 +193,12 @@ A: Yes, the entity applies a carbon price of $50 per metric ton of CO2e in its i
 
 Q: Is the greenhouse gas emissions target a gross or net target, and if net, what is the associated gross target?  
 A: The target is a net greenhouse gas emissions target of 50,000 metric tons of CO2e, with an associated gross target of 75,000 metric tons of CO2e.
+</example>
 
 Adhere strictly to this format.
 
 You will play the role of the assistant.
+
 You have access to the following pieces of context:
 <context>
 {context}
@@ -174,20 +215,39 @@ Thought: I will review the provided context to answer the question.
 Final Answer: 
 """
 
-prompt_template = """
+prompt_template_old = """
 Human: You are an AI designed to provide concise, accurate, and precise answers related to Environmental, Social, and Governance (ESG) topics. Do not include any additional commentary, chit-chat, or unnecessary information. Answer the questions directly and clearly. The answer should always be in paragraph only, do not make it in bullet points. Below are examples of the type of answers I expect:
 Example Questions and Answers:
- 
+
+<example>
 Q: What is the reporting period in which the climate-related scenario analysis was carried out? A: January 1, 2022 to December 31, 2022
 Q: What are the Scope 1 greenhouse gas emissions for the organization? A: 100,000 metric tons of CO2e
 Q: Does the entity apply a carbon price in decision-making, and if so, how? A: Yes, the entity applies a carbon price of $50 per metric ton of CO2e in its investment decisions and scenario analysis.
 Q: Is the greenhouse gas emissions target a gross or net target, and if net, what is the associated gross target? A: The target is a net greenhouse gas emissions target of 50,000 metric tons of CO2e, with an associated gross target of 75,000 metric tons of CO2e.
+</example>
 Adhere strictly to this format. 
 
 Use the following pieces of context to provide a concise answer to the question at the end. Do not summarize the results. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 <context>
 {context}
 </context>
+
+Question: {question}
+
+Assistant:
+"""
+
+prompt_template = """
+I'm going to give you a IFRS Sustainability Disclosures report. Read the report carefully, because I'm going to ask you a question about it. Here is the report.
+<report>{context}</report>
+
+Please read the report carefully and analyze all aspects.
+
+Then, I will ask you some questions about this report. Please answer based on your analysis of the report. Your answers should meet the following requirements:
+
+- Answers should be based on the content of the report itself, avoiding excessive embellishment or speculation.
+- Provide direct answer from the report. Do not include any additional commentary, chit-chat, or unnecessary information.
+- If a question goes beyond the scope discussed in the report, please clearly state that it cannot be determined from the report's content.
 
 Question: {question}
 
@@ -213,15 +273,6 @@ def get_response_llm(llm, vectorstore_faiss, query, model_choice = ''):
     answer = qa({"query": query})
     return answer['result']
 
-# def get_response_openai(vectorstore_faiss, query):
-#     """Generate response using OpenAI model and vector store."""
-#     retriever = vectorstore_faiss.as_retriever(search_type="similarity", search_kwargs={"k": 3})
-#     docs = retriever.get_relevant_documents(query)
-#     context = " ".join([doc.page_content for doc in docs])
-#     prompt = PROMPT.format(context=context, question=query)
-#     response = invoke_openai_model(prompt)
-#     return response
-
 def get_response_openai(vectorstore_faiss, query, memory):
     retriever = vectorstore_faiss.as_retriever(search_type="similarity", search_kwargs={"k": 3})
     docs = retriever.get_relevant_documents(query)
@@ -239,7 +290,7 @@ def main():
     if "model_choice" not in st.session_state:
         st.session_state.model_choice = "OpenAI GPT-4o-mini"
 
-    model_choice = st.sidebar.radio("Choose a model to generate responses:", ("Anthropic Claude 3.5 Sonnet", "Llama 3.1 405B Instruct", "AI21 Jamba-Instruct v1", "OpenAI GPT-4o-mini"))
+    model_choice = st.sidebar.radio("Choose a model to generate responses:", ("Anthropic Claude 3.5 Sonnet", "Anthropic Claude 3 Haiku", "Llama 3.1 405B Instruct", "AI21 Jamba-Instruct v1", "OpenAI GPT-4o-mini"))
 
     if model_choice != st.session_state.model_choice:
         st.session_state.model_choice = model_choice
@@ -267,9 +318,15 @@ def main():
                 full_response = ""
 
                 if model_choice == "Anthropic Claude 3.5 Sonnet":
-                    llm = get_claude_llm()
+                    llm = get_claude_llm('anthropic.claude-3-5-sonnet-20240620-v1:0')
                     model = ConversationChain(llm=llm, verbose=True, memory=st.session_state.memory)
                     result = model.predict(input=ai_prompt)
+                    cost, number_of_input_tokens, number_of_output_tokens = calculate_cost_claude('anthropic.claude-3-5-sonnet-20240620-v1:0', ai_prompt, result)
+                elif model_choice == "Anthropic Claude 3 Haiku":
+                    llm = get_claude_llm('anthropic.claude-3-haiku-20240307-v1:0')
+                    model = ConversationChain(llm=llm, verbose=True, memory=st.session_state.memory)
+                    result = model.predict(input=ai_prompt)
+                    cost, number_of_input_tokens, number_of_output_tokens = calculate_cost_claude('anthropic.claude-3-haiku-20240307-v1:0', ai_prompt, result)
                 elif model_choice == "Llama 3.1 405B Instruct":
                     llm = get_meta_llama_llm()
                     model = ConversationChain(llm=llm, verbose=True, memory=st.session_state.memory)
@@ -280,6 +337,11 @@ def main():
                 else:
                     model_id = get_ai21_llm()
                     result = invoke_bedrock_model(ai_prompt, model_id, max_tokens=4096)
+
+                if model_choice in ["Anthropic Claude 3.5 Sonnet", "Anthropic Claude 3 Haiku"]:
+                    st.write(f"Number of input tokens: {number_of_input_tokens}")
+                    st.write(f"Number of output tokens: {number_of_output_tokens}")
+                    st.write(f"### Total cost: ${cost:.6f}")
 
                 for chunk in result.split(' '):  # Simulate stream of response with milliseconds delay
                     full_response += chunk + ' '
@@ -330,8 +392,13 @@ def main():
             with st.spinner(f"Generating response with {model_choice}..."):
                 faiss_index = FAISS.load_local("faiss_index", OpenAIEmbeddings(), allow_dangerous_deserialization=True)
                 if model_choice == "Anthropic Claude 3.5 Sonnet":
-                    llm = get_claude_llm()
+                    llm = get_claude_llm('anthropic.claude-3-5-sonnet-20240620-v1:0')
                     response = get_response_llm(llm, faiss_index, user_question)
+                    cost, number_of_input_tokens, number_of_output_tokens = calculate_cost_claude('anthropic.claude-3-5-sonnet-20240620-v1:0', user_question, response)
+                elif model_choice == "Anthropic Claude 3 Haiku":
+                    llm = get_claude_llm('anthropic.claude-3-haiku-20240307-v1:0')
+                    response = get_response_llm(llm, faiss_index, user_question)
+                    cost, number_of_input_tokens, number_of_output_tokens = calculate_cost_claude('anthropic.claude-3-haiku-20240307-v1:0', user_question, response)
                 elif model_choice == "Llama 3.1 405B Instruct":
                     llm = get_meta_llama_llm()
                     response = get_response_llm(llm, faiss_index, user_question, model_choice)
@@ -350,6 +417,12 @@ Q: What are the Scope 1 greenhouse gas emissions for the organization? A: 100,00
 Q: Does the entity apply a carbon price in decision-making, and if so, how? A: Yes, the entity applies a carbon price of $50 per metric ton of CO2e in its investment decisions and scenario analysis.
 Q: Is the greenhouse gas emissions target a gross or net target, and if net, what is the associated gross target? A: The target is a net greenhouse gas emissions target of 50,000 metric tons of CO2e, with an associated gross target of 75,000 metric tons of CO2e.
 Adhere strictly to this format. Use the following context to answer the question: {context}\n\nQuestion: {user_question}""", model_id, max_tokens=4096)
+
+
+                if model_choice in ["Anthropic Claude 3.5 Sonnet", "Anthropic Claude 3 Haiku"]:
+                    st.write(f"Number of input tokens: {number_of_input_tokens}")
+                    st.write(f"Number of output tokens: {number_of_output_tokens}")
+                    st.write(f"### Total cost: ${cost:.6f}")
 
                 st.markdown(f"### {model_choice}'s Response")
                 st.write(response)
